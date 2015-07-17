46ad932ab9746364190d4067c1080342a200fe51
Haojian Wu <hokein.wu@gmail.com>
2015-07-17 18:41:03 +0800

    Fix code style.

--- src/greenworks_async_workers.h
+++ src/greenworks_async_workers.h
@@ -65,8 +65,8 @@ class CloudQuotaGetWorker : public SteamAsyncWorker {
   virtual void HandleOKCallback();
 
  private:
-	int total_bytes_;
-	int available_bytes_;
+  int total_bytes_;
+  int available_bytes_;
 };
 
 class ActivateAchievementWorker : public SteamAsyncWorker {
