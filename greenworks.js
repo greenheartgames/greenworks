bebbe8ccdd7983d940934ded95d56a8694f72005
Haojian Wu <hokein.wu@gmail.com>
2015-09-06 13:46:19 +0800

    Add missing header statements.

--- greenworks.js
+++ greenworks.js
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 Greenheart Games Pty. Ltd. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+// The source code can be found in https://github.com/greenheartgames/greenworks
 var fs = require('fs');
 
 var greenworks;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2015 Greenheart Games Pty. Ltd. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
 var assert = require("assert");
 var greenworks = require('../greenworks');
 
