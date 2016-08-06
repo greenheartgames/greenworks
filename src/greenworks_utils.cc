f24be792c2e0548e5989daacded7d65bdd5f3aec
Haojian Wu <hokein.wu@gmail.com>
2016-08-06 17:27:30 +0200

    Simplify code.

--- src/greenworks_utils.cc
+++ src/greenworks_utils.cc
@@ -27,30 +27,22 @@ namespace utils {
 
 void InitUgcMatchingTypes(v8::Handle<v8::Object> exports) {
   v8::Local<v8::Object> ugc_matching_type = Nan::New<v8::Object>();
-  ugc_matching_type->Set(Nan::New("Items").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Items));
-  ugc_matching_type->Set(Nan::New("ItemsMtx").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Items_Mtx));
-  ugc_matching_type->Set(Nan::New("ItemsReadyToUse").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Items_ReadyToUse));
-  ugc_matching_type->Set(Nan::New("Collections").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Collections));
-  ugc_matching_type->Set(Nan::New("Artwork").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Artwork));
-  ugc_matching_type->Set(Nan::New("Videos").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Videos));
-  ugc_matching_type->Set(Nan::New("Screenshots").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_Screenshots));
-  ugc_matching_type->Set(Nan::New("AllGuides").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_AllGuides));
-  ugc_matching_type->Set(Nan::New("WebGuides").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_WebGuides));
-  ugc_matching_type->Set(Nan::New("IntegratedGuides").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_IntegratedGuides));
-  ugc_matching_type->Set(Nan::New("UsableInGame").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_UsableInGame));
-  ugc_matching_type->Set(Nan::New("ControllerBindings").ToLocalChecked(),
-                         Nan::New(k_EUGCMatchingUGCType_ControllerBindings));
+  SET_TYPE(ugc_matching_type, "Items", k_EUGCMatchingUGCType_Items);
+  SET_TYPE(ugc_matching_type, "ItemsMtx", k_EUGCMatchingUGCType_Items_Mtx);
+  SET_TYPE(ugc_matching_type, "ItemsReadyToUse",
+           k_EUGCMatchingUGCType_Items_ReadyToUse);
+  SET_TYPE(ugc_matching_type, "Collections", k_EUGCMatchingUGCType_Collections);
+  SET_TYPE(ugc_matching_type, "Artwork", k_EUGCMatchingUGCType_Artwork);
+  SET_TYPE(ugc_matching_type, "Videos", k_EUGCMatchingUGCType_Videos);
+  SET_TYPE(ugc_matching_type, "Screenshots", k_EUGCMatchingUGCType_Screenshots);
+  SET_TYPE(ugc_matching_type, "AllGuides", k_EUGCMatchingUGCType_AllGuides);
+  SET_TYPE(ugc_matching_type, "WebGuides", k_EUGCMatchingUGCType_WebGuides);
+  SET_TYPE(ugc_matching_type, "IntegratedGuides",
+           k_EUGCMatchingUGCType_IntegratedGuides);
+  SET_TYPE(ugc_matching_type, "UsableInGame",
+           k_EUGCMatchingUGCType_UsableInGame);
+  SET_TYPE(ugc_matching_type, "ControllerBindings",
+           k_EUGCMatchingUGCType_ControllerBindings);
   Nan::Persistent<v8::Object> constructor;
   constructor.Reset(ugc_matching_type);
   Nan::Set(exports,
@@ -60,37 +52,26 @@ void InitUgcMatchingTypes(v8::Handle<v8::Object> exports) {
 
 void InitUgcQueryTypes(v8::Handle<v8::Object> exports) {
   v8::Local<v8::Object> ugc_query_type = Nan::New<v8::Object>();
-  ugc_query_type->Set(Nan::New("RankedByVote").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByVote));
-  ugc_query_type->Set(Nan::New("RankedByPublicationDate").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByPublicationDate));
-  ugc_query_type->Set(
-      Nan::New("AcceptedForGameRankedByAcceptanceDate").ToLocalChecked(),
-      Nan::New(k_EUGCQuery_AcceptedForGameRankedByAcceptanceDate));
-  ugc_query_type->Set(Nan::New("RankedByTrend").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByTrend));
-  ugc_query_type->Set(
-      Nan::New("FavoritedByFriendsRankedByPublicationDate").ToLocalChecked(),
-      Nan::New(k_EUGCQuery_FavoritedByFriendsRankedByPublicationDate));
-  ugc_query_type->Set(
-      Nan::New("CreatedByFriendsRankedByPublicationDate").ToLocalChecked(),
-      Nan::New(k_EUGCQuery_CreatedByFriendsRankedByPublicationDate));
-  ugc_query_type->Set(
-      Nan::New("RankedByNumTimesReported").ToLocalChecked(),
-      Nan::New(k_EUGCQuery_RankedByNumTimesReported));
-  ugc_query_type->Set(
-      Nan::New("CreatedByFollowedUsersRankedByPublicationDate")
-          .ToLocalChecked(),
-      Nan::New(k_EUGCQuery_CreatedByFollowedUsersRankedByPublicationDate));
-  ugc_query_type->Set(Nan::New("NotYetRated").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_NotYetRated));
-  ugc_query_type->Set(Nan::New("RankedByTotalVotesAsc").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByTotalVotesAsc));
-  ugc_query_type->Set(Nan::New("RankedByVotesUp").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByVotesUp));
-  ugc_query_type->Set(Nan::New("RankedByTextSearch").ToLocalChecked(),
-                      Nan::New(k_EUGCQuery_RankedByTextSearch));
-
+  SET_TYPE(ugc_query_type, "RankedByVote", k_EUGCQuery_RankedByVote);
+  SET_TYPE(ugc_query_type, "RankedByPublicationDate",
+           k_EUGCQuery_RankedByPublicationDate);
+  SET_TYPE(ugc_query_type, "AcceptedForGameRankedByAcceptanceDate",
+           k_EUGCQuery_AcceptedForGameRankedByAcceptanceDate);
+  SET_TYPE(ugc_query_type, "RankedByTrend", k_EUGCQuery_RankedByTrend);
+  SET_TYPE(ugc_query_type, "FavoritedByFriendsRankedByPublicationDate",
+           k_EUGCQuery_FavoritedByFriendsRankedByPublicationDate);
+  SET_TYPE(ugc_query_type, "CreatedByFriendsRankedByPublicationDate",
+           k_EUGCQuery_CreatedByFriendsRankedByPublicationDate);
+  SET_TYPE(ugc_query_type, "RankedByNumTimesReported",
+           k_EUGCQuery_RankedByNumTimesReported);
+  SET_TYPE(ugc_query_type, "CreatedByFollowedUsersRankedByPublicationDate",
+           k_EUGCQuery_CreatedByFollowedUsersRankedByPublicationDate);
+  SET_TYPE(ugc_query_type, "NotYetRated",  k_EUGCQuery_NotYetRated);
+  SET_TYPE(ugc_query_type, "RankedByTotalVotesAsc",
+           k_EUGCQuery_RankedByTotalVotesAsc);
+  SET_TYPE(ugc_query_type, "RankedByVotesUp", k_EUGCQuery_RankedByVotesUp);
+  SET_TYPE(ugc_query_type, "RankedByTextSearch",
+           k_EUGCQuery_RankedByTextSearch);
   Nan::Persistent<v8::Object> constructor;
   constructor.Reset(ugc_query_type);
   Nan::Set(exports,
@@ -100,24 +81,15 @@ void InitUgcQueryTypes(v8::Handle<v8::Object> exports) {
 
 void InitUserUgcList(v8::Handle<v8::Object> exports) {
   v8::Local<v8::Object> ugc_list = Nan::New<v8::Object>();
-  ugc_list->Set(Nan::New("Published").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_Published));
-  ugc_list->Set(Nan::New("VotedOn").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_VotedOn));
-  ugc_list->Set(Nan::New("VotedUp").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_VotedUp));
-  ugc_list->Set(Nan::New("VotedDown").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_VotedDown));
-  ugc_list->Set(Nan::New("WillVoteLater").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_WillVoteLater));
-  ugc_list->Set(Nan::New("Favorited").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_Favorited));
-  ugc_list->Set(Nan::New("Subscribed").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_Subscribed));
-  ugc_list->Set(Nan::New("UsedOrPlayer").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_UsedOrPlayed));
-  ugc_list->Set(Nan::New("Followed").ToLocalChecked(),
-                Nan::New(k_EUserUGCList_Followed));
+  SET_TYPE(ugc_list, "Published", k_EUserUGCList_Published);
+  SET_TYPE(ugc_list, "VotedOn", k_EUserUGCList_VotedOn);
+  SET_TYPE(ugc_list, "VotedUp", k_EUserUGCList_VotedUp);
+  SET_TYPE(ugc_list, "VotedDown", k_EUserUGCList_VotedDown);
+  SET_TYPE(ugc_list, "WillVoteLater", k_EUserUGCList_WillVoteLater);
+  SET_TYPE(ugc_list, "Favorited", k_EUserUGCList_Favorited);
+  SET_TYPE(ugc_list, "Subscribed", k_EUserUGCList_Subscribed);
+  SET_TYPE(ugc_list, "UsedOrPlayer", k_EUserUGCList_UsedOrPlayed);
+  SET_TYPE(ugc_list, "Followed", k_EUserUGCList_Followed);
   Nan::Persistent<v8::Object> constructor;
   constructor.Reset(ugc_list);
   Nan::Set(exports, Nan::New("UserUGCList").ToLocalChecked(), ugc_list);
@@ -125,20 +97,19 @@ void InitUserUgcList(v8::Handle<v8::Object> exports) {
 
 void InitUserUgcListSortOrder(v8::Handle<v8::Object> exports) {
   v8::Local<v8::Object> ugc_list_sort_order = Nan::New<v8::Object>();
-  ugc_list_sort_order->Set(Nan::New("CreationOrderDesc").ToLocalChecked(),
-      Nan::New(static_cast<int>(k_EUserUGCListSortOrder_CreationOrderDesc)));
-  ugc_list_sort_order->Set(Nan::New("CreationOrderAsc").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_CreationOrderDesc));
-  ugc_list_sort_order->Set(Nan::New("TitleAsc").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_TitleAsc));
-  ugc_list_sort_order->Set(Nan::New("LastUpdatedDesc").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_LastUpdatedDesc));
-  ugc_list_sort_order->Set(Nan::New("SubscriptionDateDesc").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_SubscriptionDateDesc));
-  ugc_list_sort_order->Set(Nan::New("VoteScoreDesc").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_VoteScoreDesc));
-  ugc_list_sort_order->Set(Nan::New("ForModeration").ToLocalChecked(),
-      Nan::New(k_EUserUGCListSortOrder_ForModeration));
+  SET_TYPE(ugc_list_sort_order, "CreationOrderDesc",
+           k_EUserUGCListSortOrder_CreationOrderDesc);
+  SET_TYPE(ugc_list_sort_order, "CreationOrderAsc",
+           k_EUserUGCListSortOrder_CreationOrderAsc);
+  SET_TYPE(ugc_list_sort_order, "TitleAsc", k_EUserUGCListSortOrder_TitleAsc);
+  SET_TYPE(ugc_list_sort_order, "LastUpdatedDesc",
+           k_EUserUGCListSortOrder_LastUpdatedDesc);
+  SET_TYPE(ugc_list_sort_order, "SubscriptionDateDesc",
+           k_EUserUGCListSortOrder_SubscriptionDateDesc);
+  SET_TYPE(ugc_list_sort_order, "VoteScoreDesc",
+           k_EUserUGCListSortOrder_VoteScoreDesc);
+  SET_TYPE(ugc_list_sort_order, "ForModeration",
+           k_EUserUGCListSortOrder_ForModeration);
   Nan::Persistent<v8::Object> constructor;
   constructor.Reset(ugc_list_sort_order);
   Nan::Set(exports,
@@ -148,32 +119,21 @@ void InitUserUgcListSortOrder(v8::Handle<v8::Object> exports) {
 
 void InitFriendFlags(v8::Handle<v8::Object> exports) {
   v8::Local<v8::Object> friend_flags = Nan::New<v8::Object>();
-  friend_flags->Set(Nan::New("None").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagNone));
-  friend_flags->Set(Nan::New("Blocked").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagBlocked));
-  friend_flags->Set(Nan::New("FriendshipRequested").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagFriendshipRequested));
-  friend_flags->Set(Nan::New("Immediate").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagImmediate));
-  friend_flags->Set(Nan::New("ClanMember").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagClanMember));
-  friend_flags->Set(Nan::New("OnGameServer").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagOnGameServer));
-  friend_flags->Set(Nan::New("RequestingFriendship").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagRequestingFriendship));
-  friend_flags->Set(Nan::New("RequestingInfo").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagRequestingInfo));
-  friend_flags->Set(Nan::New("Ignored").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagIgnored));
-  friend_flags->Set(Nan::New("IgnoredFriend").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagIgnoredFriend));
-  friend_flags->Set(Nan::New("Suggested").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagSuggested));
-  friend_flags->Set(Nan::New("ChatMember").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagChatMember));
-  friend_flags->Set(Nan::New("All").ToLocalChecked(),
-                    Nan::New(k_EFriendFlagAll));
+  SET_TYPE(friend_flags, "None", k_EFriendFlagNone);
+  SET_TYPE(friend_flags, "Blocked", k_EFriendFlagBlocked);
+  SET_TYPE(friend_flags, "FriendshipRequested",
+           k_EFriendFlagFriendshipRequested);
+  SET_TYPE(friend_flags, "Immediate", k_EFriendFlagImmediate);
+  SET_TYPE(friend_flags, "ClanMember", k_EFriendFlagClanMember);
+  SET_TYPE(friend_flags, "OnGameServer", k_EFriendFlagOnGameServer);
+  SET_TYPE(friend_flags, "RequestingFriendship",
+           k_EFriendFlagRequestingFriendship);
+  SET_TYPE(friend_flags, "RequestingInfo", k_EFriendFlagRequestingInfo);
+  SET_TYPE(friend_flags, "Ignored", k_EFriendFlagIgnored);
+  SET_TYPE(friend_flags, "IgnoredFriend", k_EFriendFlagIgnoredFriend);
+  SET_TYPE(friend_flags, "Suggested", k_EFriendFlagSuggested);
+  SET_TYPE(friend_flags, "ChatMember", k_EFriendFlagChatMember);
+  SET_TYPE(friend_flags, "All", k_EFriendFlagAll);
   Nan::Persistent<v8::Object> constructor;
   constructor.Reset(friend_flags);
   Nan::Set(exports,
