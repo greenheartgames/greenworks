ec505d6a23cc563edea97fe18615d508cf8e20e5
Haojian Wu <hokein.wu@gmail.com>
2016-08-06 14:42:04 +0200

    Add an object class for CSteamID.

---
+++ src/steam_id.cc
@@ -0,0 +1,131 @@
+
+#include "steam_id.h"
+#include "greenworks_utils.h"
+#include "v8.h"
+
+namespace greenworks {
+
+v8::Local<v8::Object> SteamID::Create(CSteamID steam_id) {
+  Nan::EscapableHandleScope scope;
+  v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>();
+  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+  SetPrototypeMethod(tpl, "isAnonymous", IsAnonymous);
+  SetPrototypeMethod(tpl, "isAnonymousGameServer", IsAnonymousGameServer);
+  SetPrototypeMethod(tpl, "isAnonymousGameServerLogin",
+                     IsAnonymousGameServerLogin);
+  SetPrototypeMethod(tpl, "isAnonymousUser", IsAnonymousUser);
+  SetPrototypeMethod(tpl, "isChatAccount", IsChatAccount);
+  SetPrototypeMethod(tpl, "isClanAccount", IsClanAccount);
+  SetPrototypeMethod(tpl, "isConsoleUserAccount", IsConsoleUserAccount);
+  SetPrototypeMethod(tpl, "isContentServerAccount", IsContentServerAccount);
+  SetPrototypeMethod(tpl, "isGameServerAccount", IsGameServerAccount);
+  SetPrototypeMethod(tpl, "isIndividualAccount", IsIndividualAccount);
+  SetPrototypeMethod(tpl, "isPersistentGameServerAccount",
+                     IsPersistentGameServerAccount);
+  SetPrototypeMethod(tpl, "isLobby", IsLobby);
+  SetPrototypeMethod(tpl, "getAccountID", GetAccountID);
+  SetPrototypeMethod(tpl, "getAccountType", GetAccountType);
+  SetPrototypeMethod(tpl, "isValid", IsValid);
+  SetPrototypeMethod(tpl, "getStaticAccountKey", GetStaticAccountKey);
+  SetPrototypeMethod(tpl, "getFriendPersonaName", GetFriendPersonaName);
+
+  SteamID* obj = new SteamID(steam_id);
+  v8::Local<v8::Object> instance = tpl->GetFunction()->NewInstance();
+  Nan::SetInternalFieldPointer(instance, 0, obj);
+  return scope.Escape(instance);
+}
+
+NAN_METHOD(SteamID::IsAnonymous) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(obj->steam_id_.BAnonAccount());
+}
+
+NAN_METHOD(SteamID::IsAnonymousGameServer) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(obj->steam_id_.BAnonGameServerAccount());
+}
+
+NAN_METHOD(SteamID::IsAnonymousGameServerLogin) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BBlankAnonAccount()));
+}
+
+NAN_METHOD(SteamID::IsAnonymousUser) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BAnonUserAccount()));
+}
+
+NAN_METHOD(SteamID::IsChatAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BChatAccount()));
+}
+
+NAN_METHOD(SteamID::IsClanAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BClanAccount()));
+}
+
+NAN_METHOD(SteamID::IsConsoleUserAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BConsoleUserAccount()));
+}
+
+NAN_METHOD(SteamID::IsContentServerAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BContentServerAccount()));
+}
+
+NAN_METHOD(SteamID::IsGameServerAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BGameServerAccount()));
+}
+
+NAN_METHOD(SteamID::IsIndividualAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.BIndividualAccount()));
+}
+
+NAN_METHOD(SteamID::IsPersistentGameServerAccount) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(
+      Nan::New(obj->steam_id_.BPersistentGameServerAccount()));
+}
+
+NAN_METHOD(SteamID::IsLobby) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.IsLobby()));
+}
+
+NAN_METHOD(SteamID::GetAccountID) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(
+      Nan::New<v8::Integer>(obj->steam_id_.GetAccountID()));
+}
+
+NAN_METHOD(SteamID::GetAccountType) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(
+      Nan::New<v8::Integer>(obj->steam_id_.GetAccountID()));
+}
+
+NAN_METHOD(SteamID::IsValid) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(Nan::New(obj->steam_id_.IsValid()));
+}
+
+NAN_METHOD(SteamID::GetStaticAccountKey) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(
+      Nan::New(utils::uint64ToString(
+          obj->steam_id_.GetStaticAccountKey())).ToLocalChecked());
+}
+
+NAN_METHOD(SteamID::GetFriendPersonaName) {
+  SteamID* obj = ObjectWrap::Unwrap<SteamID>(info.Holder());
+  info.GetReturnValue().Set(
+      Nan::New(SteamFriends()->GetFriendPersonaName(obj->steam_id_))
+          .ToLocalChecked());
+}
+
+}  // namespace greenworks
---
+++ src/steam_id.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2016 Greenheart Games Pty. Ltd. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_STEAM_ID_H_
+#define SRC_STEAM_ID_H_
+
+#include "nan.h"
+#include "steam/steam_api.h"
+
+namespace greenworks {
+
+class SteamID : public Nan::ObjectWrap {
+ public:
+  static v8::Local<v8::Object> Create(CSteamID stream_id);
+
+  static NAN_METHOD(IsAnonymous);
+  static NAN_METHOD(IsAnonymousGameServer);
+  static NAN_METHOD(IsAnonymousGameServerLogin);
+  static NAN_METHOD(IsAnonymousUser);
+  static NAN_METHOD(IsChatAccount);
+  static NAN_METHOD(IsClanAccount);
+  static NAN_METHOD(IsConsoleUserAccount);
+  static NAN_METHOD(IsContentServerAccount);
+  static NAN_METHOD(IsGameServerAccount);
+  static NAN_METHOD(IsIndividualAccount);
+  static NAN_METHOD(IsPersistentGameServerAccount);
+  static NAN_METHOD(IsLobby);
+  static NAN_METHOD(GetAccountID);
+  static NAN_METHOD(GetAccountType);
+  static NAN_METHOD(IsValid);
+  static NAN_METHOD(GetStaticAccountKey);
+  static NAN_METHOD(GetFriendPersonaName);
+
+ private:
+  explicit SteamID(CSteamID steam_id) : steam_id_(steam_id) {}
+  ~SteamID() {};
+
+  CSteamID steam_id_;
+};
+
+}  // namespace greenworks
+
+#endif  // SRC_STEAM_ID_H_
