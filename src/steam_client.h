9e4e68a7f00576d429c5de01f4440187f1f8a118
Kruithne <kruithne@gmail.com>
2020-10-12 20:21:46 +0100

    Fix parameter casing on OnGameRichPresenceJoinRequested

--- src/steam_client.h
+++ src/steam_client.h
@@ -39,7 +39,7 @@ class SteamClient {
     virtual void OnLobbyEnter(uint64 SteamIdLobby, int ChatPermissions, bool Locked, int ChatRoomEnterResponse) = 0;
     virtual void OnLobbyInvite(uint64 SteamIdUser, uint64 SteamIdLobby, uint64 GameId) = 0;
     virtual void OnGameLobbyJoinRequested(uint64 SteamIdLobby, uint64 SteamIdUser) = 0;
-    virtual void OnGameRichPresenceJoinRequested(uint64 steamIdFriend, std::string rgchConnect) = 0;
+    virtual void OnGameRichPresenceJoinRequested(uint64 steamIDFriend, std::string rgchConnect) = 0;
     virtual ~Observer() {}
   };
 
